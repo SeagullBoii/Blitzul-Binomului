0a97e1a378695e80dde2f07db9301035195656b7
Hayata._. <147236164+HayataAyana@users.noreply.github.com>
2024-05-12 21:15:02 +0300

    Ayana

---
+++ Assets/BulletProjectile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletProjectile : MonoBehaviour
+{
+    public float bulletSpeed;
+    public Rigidbody bulletRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletRigidbody.linearVelocity = transform.forward * bulletSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
